a28b238ff4aa5245bbbfadcaac26f1270f43a4b8
pedromagdev <devpedromag@gmail.com>
2024-09-26 13:18:37 +0000

    update

---
+++ components/projects/data-projects-carousel.tsx
@@ -0,0 +1,63 @@
+
+
+export const dataCarousel = [
+    {
+        name: "Vila Pedromag",
+        image: "/images/projects/project-2.jpeg",
+        images: [
+            {
+                path: ""
+            }
+        ], 
+        description: ""
+    },
+    {
+        name: "Condominio Residencial",
+        image: "/images/projects/proejct-1.jpg",
+        images: [
+            {
+                path: ""
+            }
+        ], 
+        description: ""
+    },
+    {
+        name: "Gallax Residencial",
+        image: "/images/projects/proeject-3.jpeg",
+        images: [
+            {
+                path: ""
+            }
+        ], 
+        description: ""
+    },   {
+        name: "Vila Pedromag",
+        image: "/images/projects/project-2.jpeg",
+        images: [
+            {
+                path: ""
+            }
+        ], 
+        description: ""
+    },
+    {
+        name: "Vila Pedromag",
+        image: "/images/projects/proejct-1.jpg",
+        images: [
+            {
+                path: ""
+            }
+        ], 
+        description: ""
+    },
+    {
+        name: "Vila Pedromag",
+        image: "/images/projects/proeject-3.jpeg",
+        images: [
+            {
+                path: ""
+            }
+        ], 
+        description: ""
+    }
+]
--- components/projects/modalProjects/modal.tsx
+++ components/projects/modalProjects/modal.tsx
@@ -1,25 +1,61 @@
 // ProjectModal.js
-import React from 'react';
-import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, Image } from '@nextui-org/react';
+import React from "react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  Image,
+} from "@nextui-org/react";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { dataCarousel } from "../data-projects-carousel";
 
 const ProjectModal = ({ isOpen, onClose, project }) => {
   return (
-    <Modal backdrop='blur' isOpen={isOpen} onClose={onClose}>
+    <Modal size="2xl" isOpen={isOpen} onClose={onClose}>
       <ModalContent>
-        <ModalHeader className="flex flex-col gap-1">{project.name}</ModalHeader>
+        <ModalHeader className="flex flex-col gap-1">
+          {project.name}
+        </ModalHeader>
         <ModalBody>
+          {/* <Carousel className="w-96">
+            <CarouselContent>
+            {dataCarousel.map((carousel, index) => (
+              <CarouselItem key={index} className="w-100 h-100">
+                <Image 
+                  src={carousel.image}
+                  width="100%"/>
+              </CarouselItem>
+            ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel> */}
           <Image
             alt="Imagem do Condomínio"
-            src={project.image || "https://via.placeholder.com/400"} 
+            src={project.image || "https://via.placeholder.com/400"}
             width="100%"
             height="200"
           />
           <p className="mt-2">
-            {project.description || "Descrição não disponível."} 
+            {project.description || "Carregando descrição..."}
           </p>
         </ModalBody>
         <ModalFooter>
-          <Button className="bg-app hover:bg-app-500 text-white hover:text-black" variant="light" onPress={onClose}>
+          <Button
+            className="bg-app hover:bg-app-500 text-white hover:text-black"
+            variant="light"
+            onPress={onClose}
+          >
             Fechar
           </Button>
         </ModalFooter>
